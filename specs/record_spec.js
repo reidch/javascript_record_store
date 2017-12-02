5fc2b158edd6ec2b7740cf4974205251127043be
Caroline Reid <carolinehreid@gmail.com>
2017-12-02 18:50:19 +0000

    adds 4 passing tests to Record

--- specs/record_spec.js
+++ specs/record_spec.js
@@ -8,20 +8,20 @@ describe("Record", function(){
 		record = new Record("American Dream", "LCD Soundsystem", "electro-disco-rock", 8);
 	});
 
-	xit("should have a title", function(){
+	it("should have a title", function(){
 		assert.strictEqual(record.title, "American Dream");
 	});
 
-	xit("should have an artist", function(){
+	it("should have an artist", function(){
 		assert.strictEqual(record.artist, "LCD Soundsystem");
 	});
 
-	xit("should have a genre", function(){
+	it("should have a genre", function(){
 		assert.strictEqual(record.genre, "electro-disco-rock");
 	});
 
-	xit("should have a price", function(){
-		assert.strictEqual(record.price, 9);
+	it("should have a price", function(){
+		assert.strictEqual(record.price, 8);
 	});
 
 	xit("should print out the record's properties as a string", function(){
